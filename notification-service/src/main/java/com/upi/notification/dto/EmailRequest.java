08d73726f34fac11da8112ca03b60cd8a65dd7f6
vishvjeet-developer <ub1871461@gmail.com>
2026-09-10 17:30:54 +0530

    implement notification service

---
+++ notification-service/src/main/java/com/upi/notification/dto/EmailRequest.java
@@ -0,0 +1,17 @@
+package com.upi.notification.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailRequest {
+
+    private String email;
+
+    private String link;
+
+
+}
